49b1487a55f4dfb7d29d2e4fdc621e340cafc657
Hayden Richards <hayden.richards@mcamustangs.org>
2026-02-27 09:12:20 -0600

    SQL Injection Attack Simulator Initial Commit

--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
-# Change to Passcode Puzzle Scene
-func _on_passcode_button_pressed():
-	get_tree().change_scene_to_file("res://passcode.tscn")
+# Switch to level one
+func _on_level_one_pressed() -> void:
+	get_tree().change_scene_to_file("res://levelOne.tscn")
 
-# Change to Heart Spade Puzzle Screen
-func _on_heart_spade_button_pressed():
-	get_tree().change_scene_to_file("res://heart_spade_puzzle.tscn")
+# Switch to level two
+func _on_level_two_pressed() -> void:
+	get_tree().change_scene_to_file("res://levelTwo.tscn")
+
+# Switch to level three
+func _on_level_three_pressed() -> void:
+	get_tree().change_scene_to_file("res://levelThree.tscn")
+
+# Switch to level four
+func _on_level_four_ec_pressed() -> void:
+	get_tree().change_scene_to_file("res://levelFourEC.tscn")
